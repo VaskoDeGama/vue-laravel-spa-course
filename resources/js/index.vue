e10cade90dfa854f88b352035f1f6c8e91abc424
VaskoDeGama <vasr161095@gmail.com>
2020-02-16 12:40:07 +0300

    make some beauty (add navbar)

--- resources/js/index.vue
+++ resources/js/index.vue
@@ -1,7 +1,12 @@
 <template>
     <div>
-        <router-link :to="{ name: 'home' }">Home</router-link>
-        <router-link :to="{ name: 'second' }">Second</router-link>
-        <router-view></router-view>
+        <div class="navbar bg-light border-bottom navbar-light">
+            <router-link class="navbar-brand mr-auto" :to="{ name: 'home' }">Dashboard</router-link>
+            <router-link class="btn navbar-item" :to="{ name: 'second' }">Second</router-link>
+        </div>
+        <div class="container mt-4 mb-4 pr-4 pl-4">
+            <router-view></router-view>
+        </div>
+
     </div>
 </template>
