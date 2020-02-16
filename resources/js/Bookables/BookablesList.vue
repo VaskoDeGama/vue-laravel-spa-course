daf7fd82122bce0350fc7bf04e0ab4c935eab927
VaskoDeGama <vasr161095@gmail.com>
2020-02-16 23:21:57 +0300

    add compProps and Methods, some layout

--- resources/js/Bookables/BookablesList.vue
+++ resources/js/Bookables/BookablesList.vue
@@ -1,26 +1,123 @@
 <template>
     <div>
-        <bookable-list-item item-title="Villa"
-                            item-contetn="some txt from"
-                            :item-price="10000">
-        </bookable-list-item>
-        <bookable-list-item item-title="Villa 2"
-                            item-contetn="some txt from 2"
-                            :item-price="20000">
-        </bookable-list-item>
-        <bookable-list-item item-title="Villa 3"
-                            item-contetn="some txt from 3"
-                            :item-price="30000">
-        </bookable-list-item>
+        <div v-if="isLoading">
+            Data is loading...
+        </div>
+        <div v-else>
+            <div class="row mb-4" v-for="row in rows" :key="row+'row'">
+                <div class="col"
+                     v-for="(item, column) in itemsInRow(row)"
+                     :key="'col'+column" >
+                        <bookable-list-item
+                                :item-id="item.id"
+                                :item-title="item.title"
+                                :item-contetn="item.content"
+                                :item-price="item.price" >
+                        </bookable-list-item>
+                </div>
+                <div class="col" v-for="placeHolder in placeHoldersInRow(row)" :key="placeholder +'p'">
 
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import BookableListItem from './BookablesListItem';
+
     export default {
         components: {
             BookableListItem,
+        },
+        data() {
+            return {
+                items: null,
+                isLoading: false,
+                columns: 3,
+
+            }
+        },
+        computed: {
+           rows() {
+               return this.items ? Math.ceil(this.items.length / this.columns) : 0;
+           }
+        },
+        methods: {
+            itemsInRow(row) {
+                return this.items.slice((row - 1) * this.columns, row * this.columns )
+            },
+            placeHoldersInRow(row) {
+                return this.columns - this.itemsInRow(row).length
+            }
+        },
+        created() {
+            this.isLoading = true;
+            setTimeout(() => {
+                this.items = [
+                    {
+                        id: 1,
+                        title : "Villa",
+                        content: "Some information about this villa",
+                        price: 1000,
+                    },
+                    {
+                        id: 2,
+                        title : "Villa",
+                        content: "Some information about this villa",
+                        price: 1000,
+                    },
+                    {
+                        id: 3,
+                        title : "Villa",
+                        content: "Some information about this villa",
+                        price: 1000,
+                    },
+                    {
+                        id: 4,
+                        title : "Villa",
+                        content: "Some information about this villa",
+                        price: 1000,
+                    },
+                    {
+                        id: 5,
+                        title : "Villa",
+                        content: "Some information about this villa",
+                        price: 1000,
+                    },
+                    {
+                        id: 6,
+                        title : "Villa",
+                        content: "Some information about this villa",
+                        price: 1000,
+                    },
+                    {
+                        id: 7,
+                        title : "Villa",
+                        content: "Some information about this villa",
+                        price: 1000,
+                    },
+                    {
+                        id: 8,
+                        title : "Villa",
+                        content: "Some information about this villa",
+                        price: 1000,
+                    },
+                    {
+                        id: 9,
+                        title : "Villa",
+                        content: "Some information about this villa",
+                        price: 1000,
+                    },
+                    {
+                        id: 10,
+                        title : "Villa",
+                        content: "Some information about this villa",
+                        price: 1000,
+                    }
+                ];
+                this.isLoading = false;
+            }, 3000);
         }
     }
 </script>
--- resources/js/Bookables/BookablesListItem.vue
+++ resources/js/Bookables/BookablesListItem.vue
@@ -1,14 +1,19 @@
 <template>
-    <div>
-        <h2>{{ itemTitle }}</h2>
-        <p> {{ itemContetn }}</p>
+    <div class="card">
+        <div class="card-body">
+            <h2 class="card-title">{{ itemTitle +' '+ itemId }}</h2>
+            <p class="card-text">{{ itemContetn }}</p>
+            <a href="#" class="btn btn-primary float-right">{{ itemPrice * itemId }}</a>
+        </div>
     </div>
 </template>
 
 
 <script>
+
     export default {
         props: {
+            itemId: Number,
             itemTitle: String,
             itemContetn: String,
             itemPrice: Number,
