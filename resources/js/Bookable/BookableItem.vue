ec948d9aaa17cbc58659b3701fd73ba88b324556
VaskoDeGama <vasr161095@gmail.com>
2020-02-17 16:41:23 +0300

    make representation of object

--- resources/js/Bookable/BookableItem.vue
+++ resources/js/Bookable/BookableItem.vue
@@ -1,6 +1,24 @@
 <template>
-    <div>
-        {{item.title}}
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card" >
+                <div class="card-body">
+                    <div v-if="!isLoading">
+                        <h5>{{item.title}}</h5>
+                        <hr>
+                        <article>
+                            {{item.description}}
+                        </article>
+                    </div>
+                    <div v-else>
+                        Data is loading...
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            avaliable & prices
+        </div>
     </div>
 </template>
 
@@ -9,14 +27,16 @@
         data() {
             return  {
                 item: null,
+                isLoading: false,
             }
         },
         created() {
+            this.isLoading = true;
             axios
                 .get(`/api/bookable/${this.$route.params.id}`)
                 .then(res => {
                     this.item = res.data;
-                    console.log(res.data);
+                    this.isLoading = false;
                 })
         }
     }
