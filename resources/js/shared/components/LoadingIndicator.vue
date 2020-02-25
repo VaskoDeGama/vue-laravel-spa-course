b53ca156e368cb005e71657f2ba30dbd5d05c69b
VaskoDeGama <vasr161095@gmail.com>
2020-02-25 18:41:27 +0800

    add  loading indicator component  and animated icons

---
+++ resources/js/shared/components/LoadingIndicator.vue
@@ -0,0 +1,27 @@
+<template>
+    <span>
+        <span v-if="this.loading">Check!</span>
+        <span v-if="!this.loading"><i :class="this.type"></i><slot></slot></span>
+    </span>
+</template>
+
+<script>
+    export default {
+        name: "LoadingIndicator",
+        props: {
+            loading: Boolean,
+            object: String,
+            pulse: [String,Boolean],
+        },
+        data() {
+            return{
+                type: `fas fa-${this.object || 'cog'} fa-${(this.pulse ? 'pulse' : 'spin')}`
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
