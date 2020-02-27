a87d891b6b6f3ec4784005cd6d752e1fde211e66
VaskoDeGama <vasr161095@gmail.com>
2020-02-27 20:30:45 +0800

    get price data from api

--- resources/js/bookable/BookableItem.vue
+++ resources/js/bookable/BookableItem.vue
@@ -1,31 +1,36 @@
 <template>
-    <div class="row">
-        <div class="col-md-8">
-            <div class="card mb-4" >
-                <div class="card-body">
-                    <div v-if="!isLoaded">
-                        <h5>{{item.title}}</h5>
-                        <hr>
-                        <article>
-                            {{item.description}}
-                        </article>
-                    </div>
-                    <div v-else>
-                        Data is loading...
+    <div>
+        <fatal-error v-if="error"></fatal-error>
+        <div class="row" v-else>
+            <div class="col-md-8">
+                <div class="card mb-4" >
+                    <div class="card-body">
+                        <div v-if="!loading">
+                            <h5>{{bookable.title}}</h5>
+                            <hr>
+                            <article>
+                                {{bookable.description}}
+                            </article>
+                        </div>
+                        <div v-else>
+                            Data is loading...
+                        </div>
                     </div>
                 </div>
+                <review-list v-if="!loading" :bookable-id="bookable.id"></review-list>
             </div>
-            <review-list :bookable-id="this.bookableId"></review-list>
-        </div>
-        <div class="col-md-4">
-            <availability :bookable-id="this.bookableId"  v-on:availability="checkPrice($event)"></availability>
+            <div class="col-md-4" v-if="!loading">
+                <availability :bookable-id="bookable.id"  v-on:availability="checkPrice($event)"></availability>
+            </div>
         </div>
     </div>
+
 </template>
 
 <script>
     import Availability from './Availability';
     import ReviewList from './ReviewList';
+    import {mapState} from "vuex";
     export default {
         components: {
             Availability,
@@ -33,36 +38,38 @@
         },
         data() {
             return  {
-                item: null,
-                isLoaded: false,
-                bookableId: this.$route.params.id,
+                bookable: null,
+                loading: false,
                 price: null,
-                from: this.$store.state.lastSearch.from,
-                to: this.$store.state.lastSearch.to,
+                error: null,
             }
         },
-        created() {
-            this.isLoaded = true;
-            axios
-                .get(`/api/bookables/${this.bookableId}`)
-                .then(res => {
-                    this.item = res.data.data;
-                    this.isLoaded = false;
-                })
+        async created() {
+            this.loading = true;
+            try {
+                this.bookable  = (await axios.get(`/api/bookables/${this.$route.params.id}`)).data.data;
+                this.loading = false;
+            } catch (err) {
+                this.error = err;
+            }
         },
+        computed: mapState ({
+           lastSearch: 'lastSearch'
+        }),
         methods: {
             async checkPrice(hasAvailability) {
-                if(hasAvailability) {
-                    try {
-                        this.price  = (await axios.get(`/api/bookables/${this.bookableId}/price?from=${this.from}&to=${this.to}`)).data.data;
-                        console.log(this.price);
-                    } catch (err) {
-
-                    }
+                if(!hasAvailability) {
+                    this.price = null;
+                    return;
                 }
 
+                try {
+                    this.price  = (await axios.get(`/api/bookables/${this.bookable.id}/price?from=${this.lastSearch.from}&to=${this.lastSearch.to}`)).data.data;
+                } catch (err) {
+                    this.price = null;
+                }
 
-            }
+            },
         }
     }
 </script>
