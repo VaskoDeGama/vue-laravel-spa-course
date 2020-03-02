7007e8ae07b713885de317419cf2ce7b57f4c03a
VaskoDeGama <vasr161095@gmail.com>
2020-03-02 21:49:10 +0800

    add migrations for addresses and add price and address id to bookable table

---
+++ database/migrations/2020_03_02_133223_create_addresses_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+
+            $table->string('fires_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('street');
+            $table->string('city',20);
+            $table->string('country',20);
+            $table->string('state',20);
+            $table->string('zip', 6);
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
